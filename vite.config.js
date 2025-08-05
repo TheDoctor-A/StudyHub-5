8c83333235c2fcb9696b1ca1e3de815a6c504c99
TheDoctor-A <abdul66924@gmail.com>
2025-08-05 04:43:47 +0100

    Remove node_modules and fix Tailwind config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
